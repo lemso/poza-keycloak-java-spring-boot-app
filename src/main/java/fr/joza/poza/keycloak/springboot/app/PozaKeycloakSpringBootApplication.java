cc97066f6d58b4d995f5798c641a761961c368e0
Lamine <emailtolamine@gmail.com>
2019-09-19 11:14:52 +0200

    renamed app

--- src/main/java/fr/joza/poza/keycloak/springboot/app/PozaKeycloakSpringBootApplication.java
+++ src/main/java/fr/joza/poza/keycloak/springboot/app/PozaKeycloakSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PozaKeycloakSpringBootProductApplication {
+public class PozaKeycloakSpringBootApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(PozaKeycloakSpringBootProductApplication.class, args);
+        SpringApplication.run(PozaKeycloakSpringBootApplication.class, args);
     }
 }
--- src/test/java/fr/joza/poza/keycloak/springboot/app/PozaKeycloakSpringBootApplicationTests.java
+++ src/test/java/fr/joza/poza/keycloak/springboot/app/PozaKeycloakSpringBootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PozaKeycloakSpringBootProductApplicationTests {
+public class PozaKeycloakSpringBootApplicationTests {
 
     @Test
     public void contextLoads() {
